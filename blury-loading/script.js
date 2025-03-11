0bc454800c4e7d14cf3cfebe2c6572cc76f76f73
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-03-11 22:20:17 +0600

    fixes some bugs

--- blury-loading/script.js
+++ blury-loading/script.js
@@ -2,9 +2,9 @@ const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 
 
-let load = 0
+let load = 0;
 
-let int = setInterval(bluring, 30)
+let int = setInterval(bluring, 30);
 
 function bluring(){
 
